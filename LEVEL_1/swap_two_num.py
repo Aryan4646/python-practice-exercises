9fa5de2cc0a2f9a21c74bc36182df5d53259ae5a
Aryan4646 <aryansha2004@gmail.com>
2025-05-10 18:52:06 +0530

    Two number swap without temporary variable

---
+++ LEVEL_1/swap_two_num.py
@@ -0,0 +1,9 @@
+num1 = int(input("Enter the number 1 : "))
+num2 = int(input("Enter the number 2: "))
+
+print(f"\nNumber 1 before swap: {num1}")
+print(f"Number 2 before swap: {num2}\n")
+
+num1, num2 = num2, num1
+print(f"Number 1 after swap: {num1}")
+print(f"Number 2 after swap: {num2}")
