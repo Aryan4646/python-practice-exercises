8540b8fb57a03951168a6114099e117eb63407e0
Aryan4646 <aryansha2004@gmail.com>
2025-05-20 14:03:48 +0530

    Swap Variable without temp

---
+++ Practice/Data_types_variable/swap_without_temp.py
@@ -0,0 +1,21 @@
+
+a = int(input("Enter the first variable: "))
+b = int(input("Enter the second variable: "))
+print(f"Before swap a: {a}")
+print(f"Before swap b: {b}")
+# 1st Way
+# a, b = b, a
+
+# 2nd way
+# a = a+b
+# b = a-b
+# a = a-b
+
+# 3rd way
+# a = a ^ b
+# b = b ^ a
+# a = a ^ b
+
+print()
+print(f"After swap a: {a}")
+print(f"After swap b: {b}")
