12ee83261431a4011b8a7d48eded93ccb703aacf
Aryan4646 <aryansha2004@gmail.com>
2025-05-21 19:16:20 +0530

    Count of each vowel

---
+++ Practice/Data_types_variable/vowel_count.py
@@ -0,0 +1,10 @@
+# Implement a function that takes a string input and returns the count of each vowel.
+def count_vow(input):
+    count = 0
+    for i in input:
+        if i in "aieou":
+            count += 1
+    return count
+
+input_str = input("Enter the string: ")
+print(f"The number of vowel in string {input_str} is equal to {count_vow(input_str)}.")
