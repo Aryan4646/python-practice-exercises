550696e988c214539b6816217540454f79eeac83
Aryan4646 <aryansha2004@gmail.com>
2025-09-06 19:29:51 +0530

    Minimum element of an array

---
+++ Practice/Practice/min_element_Array.py
@@ -0,0 +1,13 @@
+# Minimum element of array
+
+def min_Array(a):
+    min_element = float('inf')
+    for i in a:
+        if i < min_element:
+            min_element = i
+    return min_element
+
+arr = [12, -5, 0, 7, 33, 21, -14, 8, 99, 3]
+
+print(f"Array is as follows:\n{arr}")
+print(f"The maximum element of array: {min_Array(arr)}")
